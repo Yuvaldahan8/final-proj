e5fd3f32c65a3de85a464de2e79aa1e106fc64d7
Yuvaldahan8 <168492752+Yuvaldahan8@users.noreply.github.com>
2024-06-20 14:04:17 +0300

    Update userController.js
    הסברים על העמוד

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,16 +7,23 @@ exports.renderLogin = (req, res) => {
 };
 
 exports.login = async (req, res) => {
+    // חילוץ של המשתנים email ו-password מהגוף של הבקשה (request body)
     const { email, password } = req.body;
+   // חיפוש משתמש בבסיס הנתונים לפי כתובת האימייל שסופקה
     const user = await User.findOne({ email });
+   // אם נמצא משתמש והסיסמה שסופקה תואמת לסיסמה המוצפנת בבסיס הנתונים
     if (user && (await bcrypt.compare(password, user.password))) {
+        // שמירת פרטי המשתמש בסשן של הבקשה
         req.session.user = user;
+        
+        // בדיקת תפקיד המשתמש והפניה לדף המתאים
         if (user.role === "admin") {
             res.redirect("/admin/dashboard");
         } else {
             res.redirect("/");
         }
     } else {
+        // אם לא נמצא משתמש או הסיסמה לא תואמת, הצגת שגיאה בדף ההתחברות
         res.render("login", { error: "Invalid email or password" });
     }
 };

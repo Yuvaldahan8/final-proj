cba9e228d41e9d46d2d883cac4d1fc5e258fc8a8
Omri Ben Simon <omribensimon@outlook.co.il>
2024-07-21 20:23:37 +0300

    app.js update

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const methodOverride = require('method-override');
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const connectDB = require("./config");
 
 // Routes
 const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -27,6 +35,7 @@ app.set("view engine", "ejs");
 
 // Routes
 app.use("/", userRoutes);
+app.use("/admin", adminRoutes);
 
 // Start the server
 app.listen(3000, () => {

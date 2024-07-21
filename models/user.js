5f87c289e9b0e14574f916359210170689577d64
Komisho <komisho@walla.com>
2024-07-21 17:04:12 +0300

    added cart

--- models/user.js
+++ models/user.js
@@ -5,6 +5,8 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["admin", "user"], default: "user" },
+    cart: {type: Array , default:[]}
+    //להוסיף פה מערך של מוצרים שיש ללקוח בסל עם אפשרויות עריכה לכל מוצרוכל הדרישות שכתובות בדף על המודלים
 });
 
 module.exports = mongoose.model("User", UserSchema);

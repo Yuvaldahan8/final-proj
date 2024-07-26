9fb70c8aa298efc72dc3a1893cef6c69a0ed29c1
Sagiv9 <164031719+Sagiv9@users.noreply.github.com>
2024-07-26 18:24:03 +0300

    Mongodb atlas

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+let uri = 'mongodb+srv://sagivsa:test1234@nursery.lagwrwp.mongodb.net/?retryWrites=true&w=majority&appName=Nursery'
+
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/nursery");
+        await mongoose.connect(uri);
         console.log("MongoDB Connected...");
     } catch (e) {
         console.error(e.message);

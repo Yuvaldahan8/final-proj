34d1b73867c9b87931206bde7beb0ec5f6520981
Omri Ben Simon <omribensimon@outlook.co.il>
2024-07-31 19:16:15 +0300

    checkout adding
    adding checkoutController, checkout.css, checkout.js, checkoutRoutes and checkout.ejs

---
+++ routes/checkoutRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const checkoutController = require('../controllers/checkoutController');
+
+// נתיב GET להצגת עמוד ה-checkout
+router.get('/', checkoutController.getCheckoutPage);
+
+// נתיב POST לטיפול בהזמנה
+router.post('/checkout', checkoutController.postOrder);
+
+module.exports = router;

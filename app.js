eb73ec5ba08c30dd95383d2961e9a67d3961e674
Omri Ben Simon <omribensimon@outlook.co.il>
2024-08-01 17:10:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,7 @@ const connectDB = require("./config");
 // Routes
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-<<<<<<< Updated upstream
 const supplierroutes = require("./routes/supplierroutes");
-=======
-const supplierRoutes = require("./routes/supplierRoutes");
-const catalogRoutes = require("./routes/catalogRoutes");
-const facebookAuthRoutes = require("./routes/facebookAuth");
-const chartsRoutes = require("./routes/chartsRoutes"); 
-const checkoutRoutes = require('./routes/checkoutRoutes');
->>>>>>> Stashed changes
 
 const app = express();
 
@@ -44,20 +36,7 @@ app.set("view engine", "ejs");
 // Routes
 app.use("/", userRoutes);
 app.use("/admin", adminRoutes);
-<<<<<<< Updated upstream
 app.use("/supplier", supplierroutes);
-=======
-app.use("/supplier", supplierRoutes);
-app.use("/catalog", catalogRoutes);
-app.use(express.static('public'));
-app.use('/auth/facebook', facebookAuthRoutes);
-app.use('/charts', chartsRoutes);  // Add charts routes
-app.use('/products', productRoutes); // חיבור נתיבי המוצרים
-app.use('/cart', cartRoutes); // חיבור נתיבי העגלה
-app.use('/checkout', checkoutRoutes);
-
-
->>>>>>> Stashed changes
 
 // Start the server
 app.listen(3000, () => {

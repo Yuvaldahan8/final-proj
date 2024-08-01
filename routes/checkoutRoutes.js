9a5064e7fbeb0c6e5436c3195476b4f873326bb3
Omri Ben Simon <omribensimon@outlook.co.il>
2024-08-01 18:46:19 +0300

    remove comments

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const checkoutController = require('../controllers/checkoutController');
 
-// נתיב GET להצגת עמוד ה-checkout
 router.get('/', checkoutController.getCheckoutPage);
 
-// נתיב POST לטיפול בהזמנה
 router.post('/', checkoutController.postOrder);
 
 module.exports = router;

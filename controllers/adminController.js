94f7982e1dccf6c7fa6808f896021c4e06f938b6
yuval dahan <Yuvallldahan@gmail.com>
2024-08-01 21:32:56 +0300

    adding the users to the admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,22 +1,41 @@
 const Order = require("../models/order");
-exports.renderAdmin = async (req, res) => {
+const User = require('../models/user');
+
+exports.viewUsers = async (req, res) => {
     if (!req.session.user) {
-        res.redirect("/login?message=User is not logged in"); 
+        return res.redirect("/login?message=User is not logged in");
     }
-    else {
-        const user = req.session.user;
-        if (user.role !== 'admin') {
-            res.redirect("/login?message=User is not logged in as admin"); 
-        }
-        else {
-            let orders = [];
-            orders = await Order.find().populate(
-                "products.product"
-            );
-            res.render("admin/admin", { user, orders });
-        }
+    const user = req.session.user;
+    if (user.role !== 'admin') {
+        return res.redirect("/login?message=User is not logged in as admin");
+    }
+    try {
+        const users = await User.find(); // שליפת כל המשתמשים
+        res.render("admin/admin", { user, users }); // העברת הנתונים לדף EJS
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving users');
     }
 };
+
+exports.renderAdmin = async (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/login?message=User is not logged in");
+    }
+    const user = req.session.user;
+    if (user.role !== 'admin') {
+        return res.redirect("/login?message=User is not logged in as admin");
+    }
+    try {
+        const orders = await Order.find().populate("products.product");
+        const users = await User.find(); // שליפת כל המשתמשים
+        res.render("admin/admin", { user, orders, users });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving data');
+    }
+};
+
 
 exports.deleteOrder = async (req, res) => {
     if (!req.session.user) {
